6e360962162403efa40f379a40ab9e605b6de2eb
Megan Riel-Mehan <meganrm@gmail.com>
2018-05-25 13:48:57 -0700

    adding and playing with plotly

--- src/index.js
+++ src/index.js
@@ -1,75 +1,100 @@
-import {
-    GlobalView,
-    RandomDataset,
-} from "global-view";
 
-import Data from './AICS_Cell-feature-analysis_v1.7.csv';
+
+// import Data from './AICS_Cell-feature-analysis_v1.7.csv';
+import superagent from 'superagent';
+import Data from './cell-feature-analysis.json'
 var Plotly = require('plotly.js/dist/plotly');
-// const globalviewPlot = new GlobalView(document.querySelector('#global-view'), null);
-// const globalviewData = new RandomDataset(1000000, 3, (dataset) => globalviewPlot.load(dataset, 0, 1, 2, 2));
+import { loadImageData, make3Dviewer } from './volume-view';
 
-function normal() {
-    var x = 0,
-        y = 0,
-        rds, c;
-    do {
-        x = Math.random() * 2 - 1;
-        y = Math.random() * 2 - 1;
-        rds = x * x + y * y;
-    } while (rds == 0 || rds > 1);
-    c = Math.sqrt(-2 * Math.log(rds) / rds); // Box-Muller transform
-    return x * c; // throw away extra sample y * c
-}
+let currentX;
+let currentY;
 
-
-function init() {
-    // set_handlers("app");
-}
-
-init()
-let loaded = false;
 function onChannelDataReady() {
     let loaded = true;
     console.log("Got channel data");
 }
 processData(Data)
 
+function makeSelectors(alloptions) {
+    let xselect = document.getElementById('x-axis-select');
+    let yselect = document.getElementById('y-axis-select')
+    alloptions.forEach(ele => {
+        let xelement = document.createElement('option');
+        xelement.value = ele;
+        xelement.text = ele;
+        let yelement = document.createElement('option');
+        yelement.value = ele;
+        yelement.text = ele;
+        xselect.appendChild(xelement);
+        yselect.appendChild(yelement)
+    })
+    xselect.addEventListener('change', function(e){
+        changeData(this.id, this.value)
+    })
+    yselect.addEventListener('change', function (e) {
+        changeData(this.id, this.value)
+    })
+};
+
+function changeData(axisId, value){
+    let axis = axisId.split('-')[0];
+    let newx;
+    let newy;
+    if (axis === 'x'){
+        newx = value;
+        currentX = value;
+        newy = document.getElementById('y-axis-select').value;
+    }
+    if (axis === 'y') {
+        newy = value;
+        currentY = value;
+        newx = document.getElementById('x-axis-select').value;
+
+    }
+    makePlotly(Data, newx, newy, 'structureProteinName');
+}
 
 function processData(allRows) {
 
-    let x = []
-    let y = []
-    let classes = [];
-    let names = [];
+    let alloptions = Object.keys(allRows[0]).filter(ele => ele !== 'structureProteinName' && ele !== 'Cell ID' && ele !== 'datadir');
 
-    for (var i = 0; i < allRows.length; i++) {
-        let row = allRows[i];
-        x.push(row['Nuclear volume (fL)'])
-        y.push(row['Cellular volume (fL)'])
-        classes.push(row['classes']);
-        names.push(row['Cell ID'])
-    }
-    makePlotly(x, y, classes, names);
+    makeSelectors(alloptions);
+    currentX = 'Nuclear volume (fL)';
+    currentY = 'Cellular volume (fL)';
+    makePlotly(allRows, 'Nuclear volume (fL)', 'Cellular volume (fL)', 'structureProteinName');
 }
 
-function makePlotly(x, y, classes, names) {
+function unpack(rows, key) {
+    return rows.map(function (row) { return row[key]; });
+}
+
+function makePlotly(allRows, plotbyOnX, plotByOnY, ColorBy) {
     var trace1 = {
-        x: x,
-        y: y,
-        names: names,
+        x: unpack(allRows, plotbyOnX),
+        y: unpack(allRows, plotByOnY),
+        names: unpack(allRows, 'Cell ID'),
+        dir: unpack(allRows, 'datadir'),
         mode: 'markers',
+        name: 'scatterplot',
         marker: {
             symbol: 'circle',
             size: 4,
             opacity: 0.4
         },
-        type: 'scattergl'
+        type: 'scattergl',
+        legendgroup: ColorBy,
+
+        transforms: [
+            {
+                type: 'groupby',
+                groups: unpack(allRows, ColorBy),
+    
+            }]
     };
 
-
     var trace2 = {
-        x: x,
-        y: y,
+        x: unpack(allRows, plotbyOnX),
+        y: unpack(allRows, plotByOnY),
         name: 'density',
         ncontours: 20,
         colorscale: 'Hot',
@@ -78,20 +103,22 @@ function makePlotly(x, y, classes, names) {
         type: 'histogram2dcontour'
     };
     var trace3 = {
-        x: x,
+        x: unpack(allRows, plotbyOnX),
         name: 'x density',
         marker: { color: 'rgb(102,0,0)' },
         yaxis: 'y2',
         type: 'histogram'
     };
     var trace4 = {
-        y: y,
+        y: unpack(allRows, plotByOnY),
         name: 'y density',
         marker: { color: 'rgb(102,0,0)' },
         xaxis: 'x2',
+        autobiny: false,
+        nbinsy: 5,
         type: 'histogram'
     };
-    var data = [trace4, trace3, trace2, trace1];
+    var data = [trace1];
 
 
     var layout = {
@@ -127,16 +154,114 @@ function makePlotly(x, y, classes, names) {
     const plot = document.getElementById('app')
     const hoverInfo = document.getElementById('hoverinfo')
     plot.on('plotly_hover', function (hovered) {
-        console.log(hovered.points[0])
-        var infotext = hovered.points.map(function (d) {
-            return (d.data.names[d.pointIndex])
-            // return ('name: ' + xaxis.l2p(d.x) + ', height: ' + yaxis.l2p(d.y));
-        });
-
-        hoverInfo.innerHTML = infotext.join('');
+        let point = hovered.points[0];
+        var thumbnailSrc = point.data.dir ? point.data.dir[point.pointIndex] : null;
+        var cellID = point.data.names? point.data.names[point.pointIndex] : null;
+        if (cellID && thumbnailSrc ){
+            let cellLineId = cellID.split('_')[0];
+            hoverInfo.children[0].setAttribute('src', `https://cellviewer-1-2-0.allencell.org/aics/thumbnails/${thumbnailSrc}/${cellLineId}/${cellID}.png`)
+            hoverInfo.style.left = hovered.event.pointerX + 'px';
+            hoverInfo.style.top = hovered.event.pointerY + 'px';
+        }
     })
         .on('plotly_unhover', function (data) {
-            hoverInfo.innerHTML = '';
-        });
+            hoverInfo.children[0].setAttribute('src', '')
+        })
+        .on('plotly_click', function (clicked) {
+            let points = clicked.points;
+            points.map(point => {
 
+                if (point.data.name === 'scatterplot'){
+                    var thumbnailSrc = point.data.dir ? point.data.dir[point.pointIndex] : null;
+                    var cellID = point.data.names ? point.data.names[point.pointIndex] : null;
+
+                    if (cellID && thumbnailSrc) {
+                        let cellLineId = cellID.split('_')[0];
+                        // PREPARE SOME TEST DATA TO TRY TO DISPLAY A VOLUME.
+                        // let imgdata = {
+                        //     "width": 306,
+                        //     "height": 494,
+                        //     "channels": 9,
+                        //     "channel_names": ["DRAQ5", "EGFP", "Hoechst 33258", "TL Brightfield", "SEG_STRUCT", "SEG_Memb", "SEG_DNA", "CON_Memb", "CON_DNA"],
+                        //     "rows": 7,
+                        //     "cols": 10,
+                        //     "tiles": 65,
+                        //     "tile_width": 204,
+                        //     "tile_height": 292,
+                        //     "atlas_width": 2040,
+                        //     "atlas_height": 2044,
+                        //     "pixel_size_x": 0.065,
+                        //     "pixel_size_y": 0.065,
+                        //     "pixel_size_z": 0.29,
+                        //     "images": [{
+                        //         "name": "examples/AICS-10_5_5.ome.tif_atlas_0.png",
+                        //         "channels": [0, 1, 2]
+                        //     }, {
+                        //         "name": "examples/AICS-10_5_5.ome.tif_atlas_1.png",
+                        //         "channels": [3, 4, 5]
+                        //     }, {
+                        //         "name": "examples/AICS-10_5_5.ome.tif_atlas_2.png",
+                        //         "channels": [6, 7, 8]
+                        //     }],
+                        //     "name": "AICS-10_5_5",
+                        //     "status": "OK",
+                        //     "version": "0.0.0",
+                        //     "aicsImageVersion": "0.3.0"
+                        // };
+                        
+                        const url = `https://s3-us-west-2.amazonaws.com/bisque.allencell.org/v1.2.0/Cell-Viewer_Thumbnails/${thumbnailSrc}/${cellLineId}/${cellID}_atlas.json`;
+                        return superagent.get(url)
+                            .then(res => res.body)
+                            .then(jsonData => {
+                                let newUrls = jsonData.images.map(image => {
+                                    image.name = `https://cellviewer-1-2-0.allencell.org/aics/thumbnails/${thumbnailSrc}/${cellLineId}/${image.name}`;
+                                    return image
+                                });
+                                jsonData.images = newUrls
+                                return jsonData;
+                            })
+                            .then(loadImageData)
+
+                    }
+                }
+            })
+
+    })
+    .on('plotly_relayout',
+        function (eventdata) {
+
+            if (eventdata['yaxis.range[1]'] - eventdata['yaxis.range[0]'] < 500){
+                let xvalues = unpack(allRows, currentX).map((ele, index) => {
+                    if (ele < eventdata['xaxis.range[1]'] && ele > eventdata['xaxis.range[0]']) {
+
+                        return ele;
+                    }
+                    return false 
+                })
+                let yvalues = unpack(allRows, currentY).map((ele, index) => {
+                    if (ele < eventdata['yaxis.range[1]'] && ele > eventdata['yaxis.range[0]']) {
+                        return ele;
+                    }
+                    return false
+                })
+                const currentPoints = xvalues.reduce((acc, cur, index) => {
+                    if (cur && yvalues[index]){
+                        let obj = {};
+                        obj.x = cur;
+                        obj.y = yvalues[index];
+                        acc.push(obj);
+                    }
+                    return acc
+                }, [])
+                let xbounds = {max: eventdata['xaxis.range[1]'], min: eventdata['xaxis.range[0]']}
+                let ybounds = { max: eventdata['yaxis.range[1]'] , min: eventdata['yaxis.range[0]']}
+
+                // if(currentPoints.length < 4){
+                //     currentPoints.forEach(point => {
+                //         make3Dviewer(point, xbounds, ybounds);
+                //     })
+                // }
+            }
+        });
 }
+
--- src/volume-view.js
+++ src/volume-view.js
@@ -1,6 +1,24 @@
 const VolumeViewer = require('volume-viewer');
-const viewer = document.getElementById('viewer')
-let view3D = new VolumeViewer.AICSview3d(viewer);
+const viewer = document.getElementById('viewer');
+import control from 'control-panel';
+let mainview3D = new VolumeViewer.AICSview3d(viewer);
+
+export function make3Dviewer(point, xbounds, ybounds) {
+    const viewerContainer = document.getElementById('zoom-viewers')
+
+    const newViewer = document.createElement('div');
+    newViewer.id = `${point.x}-${point.y}`;
+    newViewer.classList.add('little-viewer')
+    newViewer.style.position = 'absolute';
+    newViewer.style.right = ((point.x - xbounds.min)/(xbounds.max - xbounds.min)) + 'px';
+    newViewer.style.top = ((point.y - ybounds.min) / (ybounds.max - ybounds.min)) + 'px';
+
+    viewerContainer.appendChild(newViewer)
+    let view3D = new VolumeViewer.AICSview3d(newViewer);
+
+    loadImageData(imgdata, view3D)
+}
+
 const defaultColors = [
     [226, 205, 179],
     [111, 186, 17],
@@ -50,68 +68,59 @@ const colorFromArray = (val) => {
     return 'rgb(' + val[0] + ',' + val[1] + ',' + val[2] + ')'
 }
 
-// PREPARE SOME TEST DATA TO TRY TO DISPLAY A VOLUME.
-let imgdata = {
-    "width": 306,
-    "height": 494,
-    "channels": 9,
-    "channel_names": ["DRAQ5", "EGFP", "Hoechst 33258", "TL Brightfield", "SEG_STRUCT", "SEG_Memb", "SEG_DNA", "CON_Memb", "CON_DNA"],
-    "rows": 7,
-    "cols": 10,
-    "tiles": 65,
-    "tile_width": 204,
-    "tile_height": 292,
-    "atlas_width": 2040,
-    "atlas_height": 2044,
-    "pixel_size_x": 0.065,
-    "pixel_size_y": 0.065,
-    "pixel_size_z": 0.29,
-    "images": [{
-        "name": "examples/AICS-10_5_5.ome.tif_atlas_0.png",
-        "channels": [0, 1, 2]
-    }, {
-        "name": "examples/AICS-10_5_5.ome.tif_atlas_1.png",
-        "channels": [3, 4, 5]
-    }, {
-        "name": "examples/AICS-10_5_5.ome.tif_atlas_2.png",
-        "channels": [6, 7, 8]
-    }],
-    "name": "AICS-10_5_5",
-    "status": "OK",
-    "version": "0.0.0",
-    "aicsImageVersion": "0.3.0"
-};
-
-function pointermove_handler(ev) {
-    if (plot.coordSys.axes[0].maximum - plot.coordSys.axes[0].minimum < .2) {
-        console.log('zoomed in')
-        if (!loaded) {
-            loadImageData(imgdata);
-        }
-
-    } else {
-        loaded = false;
-        console.log('not zoomed')
-    }
-}
-
-function set_handlers(name) {
-    // Install event handlers for the given element
-    var el = document.getElementById(name);
-    // el.addEventListener('mousewheel', pointermove_handler)
+function onChannelDataReady(){
 
 }
+// let imgdata = {
+//     "width": 306,
+//     "height": 494,
+//     "channels": 9,
+//     "channel_names": ["DRAQ5", "EGFP", "Hoechst 33258", "TL Brightfield", "SEG_STRUCT", "SEG_Memb", "SEG_DNA", "CON_Memb", "CON_DNA"],
+//     "rows": 7,
+//     "cols": 10,
+//     "tiles": 65,
+//     "tile_width": 204,
+//     "tile_height": 292,
+//     "atlas_width": 2040,
+//     "atlas_height": 2044,
+//     "pixel_size_x": 0.065,
+//     "pixel_size_y": 0.065,
+//     "pixel_size_z": 0.29,
+//     "images": [{
+//         "name": "examples/AICS-10_5_5.ome.tif_atlas_0.png",
+//         "channels": [0, 1, 2]
+//     }, {
+//         "name": "examples/AICS-10_5_5.ome.tif_atlas_1.png",
+//         "channels": [3, 4, 5]
+//     }, {
+//         "name": "examples/AICS-10_5_5.ome.tif_atlas_2.png",
+//         "channels": [6, 7, 8]
+//     }],
+//     "name": "AICS-10_5_5",
+//     "status": "OK",
+//     "version": "0.0.0",
+//     "aicsImageVersion": "0.3.0"
+// };
+const defaultChannels = [0,1,2]
 
-function loadImageData(jsondata) {
+export function loadImageData(jsondata, ele) {
+    let view3D = ele ? ele: mainview3D;
     view3D.resize();
     const aimg = new VolumeViewer.AICSvolumeDrawable(jsondata, "test");
 
+
+    var inputs = [
+        { type: 'range', label: 'brightness', min: 0, max: 3, initial: 1 },
+        { type: 'range', label: 'density', min: 0, max: 1, initial: 0.1 },
+        { type: 'checkbox', label: 'maxProject', initial: false }
+    ]
+
     aimg.channel_names.forEach(function (d, i) {
         var colors = [colorFromArray(getColorByChannelIndex(i)), colorFromArray(getColorByChannelIndex(i))]
-        // inputs.push({ type: 'multibox', label: d, count: 2, names: ['vol', 'iso'], colors: colors, initial: [true, false] })
+        inputs.push({ type: 'multibox', label: d, count: 2, names: ['volume', 'surface'], colors: colors, initial: [i, false] })
     })
 
-    // var panel = control(inputs, { theme: 'dark', position: 'top-right', width: '250px' })
+    var panel = control(inputs, { theme: 'dark', position: 'top-right', width: '250px' })
 
     view3D.setCameraMode('3D');
     view3D.setImage(aimg, onChannelDataReady);
@@ -123,35 +132,24 @@ function loadImageData(jsondata) {
         aimg.updateChannelColor(i, getColorByChannelIndex(i))
     })
 
-    // panel.on('input', function (data) {
-    //     aimg.setBrightness(data['brightness'])
-    //     aimg.setDensity(data['density'])
-    //     aimg.setUniform('maxProject', data['maxProject'] ? 1 : 0, true, true)
+    panel.on('input', function (data) {
+        aimg.setBrightness(data['brightness'])
+        aimg.setDensity(data['density'])
+        aimg.setUniform('maxProject', data['maxProject'] ? 1 : 0, true, true)
 
-    //     aimg.channel_names.forEach(function (d, i) {
-    //         aimg.setVolumeChannelEnabled(i, data[d][0])
-    //         if (aimg.hasIsosurface(i)) {
-    //             if (!data[d][1]) {
-    //                 aimg.destroyIsosurface(i)
-    //             }
-    //         }
-    //         else {
-    //             if (data[d][1]) {
-    //                 aimg.createIsosurface(i, 50, 1);
-    //             }
-    //         }
-    //     })
-    //     aimg.fuse()
-    // })
-}
-
-function init() {
-    set_handlers("app");
-}
-
-init()
-let loaded = false;
-function onChannelDataReady() {
-    let loaded = true;
-    console.log("Got channel data");
+        aimg.channel_names.forEach(function (d, i) {
+            aimg.setVolumeChannelEnabled(i, data[d][0])
+            if (aimg.hasIsosurface(i)) {
+                if (!data[d][1]) {
+                    aimg.destroyIsosurface(i)
+                }
+            }
+            else {
+                if (data[d][1]) {
+                    aimg.createIsosurface(i, 50, 1);
+                }
+            }
+        })
+        aimg.fuse()
+    })
 }
